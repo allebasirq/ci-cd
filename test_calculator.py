cfa3a88fd6267894597fa22e4676a815af6b8dbf
Isabella Rodrigues Quesada <allebasirq@gmail.com>
2026-09-06 16:45:52 -0400

    feature: added division and multiplication functions

--- test_calculator.py
+++ test_calculator.py
@@ -1,8 +1,19 @@
 from calculator import add
 from calculator import subtract
+from calculator import multiply
+from calculator import divide
 
 def test_add():
     assert add(2, 3) == 5
 
 def test_subtract():
     assert subtract(5, 1) == 4
+
+def test_multiply():
+    assert multiply(3, 4) == 12
+
+def test_divide():
+    assert divide(10, 2) == 5
+
+def test_divide_by_zero():
+    assert divide(7, 0) == "Error: cannot divide by zero"
